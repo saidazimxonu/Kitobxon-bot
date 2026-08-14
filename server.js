c1b75d090f17d98073a60c625b928db40ed0944b
saidazimxonu <saidazimxonumurzoqov@gmail.com>
2026-08-14 16:17:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -81,6 +81,13 @@ bot.onText(/\/stop/, (msg) => {
 // ---------- Admin uchun HTTP API (e'lon yuborish) ----------
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send(`Kitobxon bot ishlab turibdi. Ro'yxatdagi foydalanuvchilar: ${Object.keys(users).length}`);
